0c6706e98ab877935768c181223a8fe1acb79df9
shahida <sherinshifana1997@gmail.com>
2018-01-19 17:15:36 +0530

    third commit

--- js/h5p-image-grid.js
+++ js/h5p-image-grid.js
@@ -1,125 +1,62 @@
-
-//code starts here
 H5P.ImageGrid = (function($,EventDispatcher,UI){
 
-        //variable declaration part
-        var self = this;
+      var self = this;
 
-        //constructor
-        function ImageGrid(params,id){
-                //constructor code goes here
-                self.params = params;
-                self.id = id;
+      function ImageGrid(params,id){
 
-                var imageLoaded = function(){
-                }
-
-
-// imagePieces now contains data urls of all the pieces of the image
-
-// load one piece onto the page
-
-}//your code goes here
+        self.params = params;
+        self.id = id;
+        var levelsNum=self.params.levels;
+        var imageHeight=self.params.image.height;
+        var imageWidth=self.params.image.width;
+        var heightOfOnePiece=self.params.image.height / self.params.levels;
+        var widthOfOnePiece=self.params.image.width / self.params.levels;
 
 
-                //when image loaded
-                //imageLoaded();
+        var getFragments = function() {
 
-                var redrawContainer = function($container,heightOfOnePiece,widthOfOnePiece,levelsNum){
-                  console.log("working");
-                  $container.empty();
-
-
-                  for(var i=0; i<levelsNum ; i++)
+            var fragmentsToUse = [];
+            for(var i=0;i<levelsNum; i++)
+            {
+                  for(j=0;j<levelsNum;j++)
                   {
-                      var newDiv=$('<div class="grid-image-div" ></div>');
-                      for(var j=0; j<levelsNum; j++)
-                      {
-                          var changeInHeight = i*heightOfOnePiece;
-                          var changeInWidth = j*widthOfOnePiece;
-                          ul.append($('<li class="li-class"><div></li>').css('background-image','url(' + H5P.getPath(self.params.image.path,self.id) + ')').css('background-position-x',-changeInWidth+'px').css('background-position-y',-changeInHeight+'px').css('height',heightOfOnePiece+'px').css('width',widthOfOnePiece+'px'));
-                      }
-                      newDiv.appendTo($(ul));
+                      var changeInHeight = i*heightOfOnePiece;
+                      var changeInWidth = j*widthOfOnePiece;
+                      // var fragment = new ImageGrid.fragments(self.params.image,self.id,heightOfOnePiece,widthOfOnePiece,changeInHeight,changeInWidth);
                   }
+            }
+            // fragmentsToUse.push(fragment);
 
 
-                }
+        }
+      }
 
-
+        // var fragmentsToUse = getFragments();
 
         ImageGrid.prototype.attach = function($container){
-                //attach function
-                var levelsNum=self.params.levels;
-                var imageHeight=self.params.image.height;
-                var imageWidth=self.params.image.width;
-                var heightOfOnePiece=self.params.image.height / self.params.levels;
-                var widthOfOnePiece=self.params.image.width / self.params.levels;
-                $container.addClass('h5p-image-grid');
-                //$container.append('<img src="'+ H5P.getPath(self.params.image.path,self.id)+'"/>');
-                if(self.params.chooseDifficulty==='true')
+            $container.addClass('h5p-image-grid');
+            ul=$('<ul class="ul-class">');
+
+            for(var i=0; i<self.params.levels ; i++)
+            {
+                var newDiv=$('<div class="grid-image-div" ></div>');
+                for(var j=0; j<self.params.levels; j++)
                 {
-                    ul=$('<ul class="ul-class">');
 
-                    for(var i=0; i<levelsNum ; i++)
-                    {
-                        var newDiv=$('<div class="grid-image-div" ></div>');
-                        for(var j=0; j<levelsNum; j++)
-                        {
-                            var changeInHeight = i*heightOfOnePiece;
-                            var changeInWidth = j*widthOfOnePiece;
-                            ul.append($('<li class="li-class"><div></li>').css('background-image','url(' + H5P.getPath(self.params.image.path,self.id) + ')').css('background-position-x',-changeInWidth+'px').css('background-position-y',-changeInHeight+'px').css('height',heightOfOnePiece+'px').css('width',widthOfOnePiece+'px'));
-                        }
-                        newDiv.appendTo($(ul));
-                    }
-                    $container.append(ul);
-
-                    $container.append('<div>Difficulty:    <select id="mySelect"><option value="3">9 pieces</option><option value="4">16 pieces</option><option value="5">25 pieces</option><option value="6">36 pieces</option><option value="7">49 pieces</option></select></div><br />');
-                    var obj=document.getElementById('mySelect');
-                    $(obj).change(function(){
-                      heightOfOnePiece=self.params.image.height/obj.value;
-                      widthOfOnePiece=self.params.image.width/obj.value;
-                      levelsNum=obj.value;
-                      console.log(heightOfOnePiece);
-
-                      redrawContainer($(ul),heightOfOnePiece,widthOfOnePiece,levelsNum);
-
-                    });
-
-
-                    self.$start=UI.createButton({
-                        title: 'Button',
-                        'class': 'level-next-button',
-                        'text' : 'Start the Puzzle',
-                      }).appendTo($container);
+                    fragmentsToUse.appendTo($(newDiv));
                 }
-                else {
-                    ul=$('<ul class="ul-class">');
-                    for(var i=0; i<levelsNum ; i++)
-                    {
-                        var newDiv=$('<div class="grid-image-div" ></div>');
-                        for(var j=0; j<levelsNum; j++)
-                        {
-                            var changeInHeight = i*heightOfOnePiece;
-                            var changeInWidth = j*widthOfOnePiece;
-                            ul.append($('<li class="li-class"><div></li>').css('background-image','url(' + H5P.getPath(self.params.image.path,self.id) + ')').css('background-position-x',-changeInWidth+'px').css('background-position-y',-changeInHeight+'px').css('height',heightOfOnePiece+'px').css('width',widthOfOnePiece+'px'));
-                        }
-                        newDiv.appendTo($(ul));
-                    }
-                    $container.append(ul);
+                newDiv.appendTo($(ul));
+            }
+            $container.append(ul);
 
-                    self.$start=UI.createButton({
-                      title: 'Button',
-                      'class': 'game-start-button',
-                      'text' : 'Start the Puzzle',
-                    }).appendTo($container);
-              }
+            self.$start=UI.createButton({
+                title: 'Button',
+                'class': 'level-next-button',
+                'text' : 'Start the Puzzle',
+              }).appendTo($container);
+        };
 
-              console.log(Math.ceil(heightOfOnePiece));
-              console.log(Math.ceil(widthOfOnePiece));
-              console.log(levelsNum);
-              H5P.trigger("resize");
-        }
-
-        return ImageGrid;
+      ImageGrid.prototype.constructor = ImageGrid;
+      return ImageGrid;
 
 })(H5P.jQuery,H5P.EventDispatcher,H5P.JoubelUI);
