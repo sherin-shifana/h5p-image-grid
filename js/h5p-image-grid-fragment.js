a6be0b7fa2b45171324255f75a717001a2920c8b
shahida <sherinshifana1997@gmail.com>
2018-01-20 23:12:35 +0530

    second commit

--- js/h5p-image-grid-fragment.js
+++ js/h5p-image-grid-fragment.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function(ImageGrid,EventDispatcher,$){
 
     ImageGrid.Fragment=function(image,id,heightOfOnePiece,widthOfOnePiece,changeInHeight,changeInWidth){
@@ -11,3 +12,20 @@ function(ImageGrid,EventDispatcher,$){
     };
 
 }(H5P.ImageGrid, H5P.EventDispatcher, H5P.jQuery);
+=======
+(function(ImageGrid,$){
+
+        ImageGrid.Fragment= function(width,height,row,col){
+
+                this.width = width;
+                this.height = height;
+                this.row = row;
+                this.col = col;
+                console.log(row+"_"+col);
+        }
+
+        ImageGrid.Fragment.prototype.constructor = ImageGrid.Fragment;
+
+        return ImageGrid.Fragment;
+})(H5P.ImageGrid,H5P.jQuery);
+>>>>>>> 388710a6d20fc1296225498637ce5cdce7647367
--- js/h5p-image-grid.js
+++ js/h5p-image-grid.js
@@ -1,5 +1,6 @@
 H5P.ImageGrid = (function($,EventDispatcher,UI){
 
+<<<<<<< HEAD
       var self = this;
 
       function ImageGrid(params,id){
@@ -12,9 +13,51 @@ H5P.ImageGrid = (function($,EventDispatcher,UI){
         var heightOfOnePiece=self.params.image.height / self.params.levels;
         var widthOfOnePiece=self.params.image.width / self.params.levels;
 
+=======
+//code starts here
+H5P.ImageGrid = (function($,UI){
+
+        //variable declaration part
+        var self = this;
+        var fragmentList = [];
+        var fragmentWidth,fragmentHeight;
+        //constructor
+        function ImageGrid(params,id){
+                //constructor code goes here
+                self.params = params;
+
+                self.id = id;
+                var level = params.levels;
+                var imageHeight = params.image.height;
+                var imageWidth = params.image.width;
+                var margin = 2;
+                var createImageGrid = function(){
+
+                        var gridWidth = parseInt(imageWidth)+(margin*level*2);
+                        var gridHeight = parseInt(imageHeight)+(margin*level*2);
+
+                        fragmentWidth = Math.floor(imageWidth/level);
+                        fragmentHeight = Math.floor(imageHeight/level);
+                        for(var i=0;i<level;i++){
+                                for(var j=0;j<level;j++)
+                                {
+                                        fragmentList.push(new ImageGrid.Fragment(fragmentWidth,fragmentHeight,i,j));
+                                 }
+
+
+                         }
+
+
+                }
+                var imageLoaded = function(){
+                        //
+                        // console.log("calling");
+                        createImageGrid();
+>>>>>>> 388710a6d20fc1296225498637ce5cdce7647367
 
         var getFragments = function() {
 
+<<<<<<< HEAD
             var fragmentsToUse = [];
             for(var i=0;i<levelsNum; i++)
             {
@@ -30,10 +73,45 @@ H5P.ImageGrid = (function($,EventDispatcher,UI){
 
         }
       }
+=======
+                self.constructGrid = function(){
+
+                var $grid = $('<ul class="grid-container" >');
+
+                  for(var i=0;i<level;i++){
+
+                    var $row = $('<div class="row" />');
+                    for(var j=0;j<level;j++){
+
+                      var $element= $("<li />").
+                                   css("background-position","-" + (fragmentList[i*level+j].width*fragmentList[i*level+j].col) +"px -" +(fragmentList[i*level+j].height*fragmentList[i*level+j].row) +"px")
+                                   .css("width", fragmentWidth+"px")
+                                   .css("height", fragmentHeight+"px")
+                                   .css("background-image", "url("+H5P.getPath(self.params.image.path,self.id)+")")
+                                   .css("display", "inline-block")
+                                   .css("margin", 1);
+
+
+
+                      $row.append($element);
+
+                    }
+                    $grid.append($row)
+                  }
+
+                  return $grid;
+                }
+
+
+                //when image loaded
+                imageLoaded();
+                // H5P.shuffleArray(fragmentList);
+>>>>>>> 388710a6d20fc1296225498637ce5cdce7647367
 
         // var fragmentsToUse = getFragments();
 
         ImageGrid.prototype.attach = function($container){
+<<<<<<< HEAD
             $container.addClass('h5p-image-grid');
             ul=$('<ul class="ul-class">');
 
@@ -60,3 +138,49 @@ H5P.ImageGrid = (function($,EventDispatcher,UI){
       return ImageGrid;
 
 })(H5P.jQuery,H5P.EventDispatcher,H5P.JoubelUI);
+=======
+                //attach function
+                $container.addClass('h5p-image-grid');
+                // $container.append('<img src="'+ H5P.getPath(self.params.image.path,self.id)+'"/>');
+
+
+               var $grid=self.constructGrid();
+
+
+
+                //ifuser can select the difficulty
+                // code goes here
+
+                //allow fullscreen
+
+                //display the start puzzle button
+
+        //         self.$submit = UI.createButton({
+        //                     title: 'Submit',
+        //   click: function(event) {
+        //     var order = $list.sortable("toArray");
+        //     gameSubmitted(order, $list);
+        //   },
+        //   html: '<span><i class="fa fa-check" aria-hidden="true"></i></span>&nbsp;' + parameters.l10n.checkAnswer
+        // });
+        // self.$submit.appendTo(self.$buttonContainer);
+
+                var $startPuzzleButton = UI.createButton({
+                  title: 'Submit',
+                  click: function(event){
+                    alert("working");
+                  },
+                  html: 'Start the puzzle'
+                });
+
+                $container.append($grid);
+                $container.append($startPuzzleButton);
+
+
+                H5P.trigger("resize");
+
+        }
+
+        return ImageGrid;
+})(H5P.jQuery,H5P.JoubelUI);
+>>>>>>> 388710a6d20fc1296225498637ce5cdce7647367
